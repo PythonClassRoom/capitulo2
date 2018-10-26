9492953ecdf1ff232cbc0d22a204805720b4bacc
Erick Salas-Chaverri <ericksc@gmail.com>
2018-10-26 14:41:05 -0600

    lista slicing ejemplos

---
+++ ejemplos_listas.py
@@ -0,0 +1,11 @@
+# Imprimir contenido
+li = [2, 'a', 4]
+
+for ele in li:
+    print(ele)
+
+#insercion y borrando
+
+li.append('nuevo')
+
+li.insert(3, 'c')
---
+++ listas_slicing_ejemplos.py
@@ -0,0 +1,14 @@
+# creacion de lista para hacer pruebas
+mi_lista = ['pera', 'banano', 'piña', 'manzana']
+
+# forma de llamar los elementos de la lista en reversa
+['manzana', 'piña', 'banano', 'pera'] == mi_lista[::-1]
+
+#forma de seleccionar los elementos de indice par
+['pera', 'piña'] == mi_lista[::2]
+
+#forma de seleccionar los elementos de indice impar
+['pera', 'piña'] == mi_lista[1::2]
+
+#sublista en reversa
+['pera', 'banano', 'piña'] == mi_lista[-5:-1]
